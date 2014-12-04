8f952523c4f1a021b31ded2126dcca8c619cf7a0
Nathan Cazell <nathancazell@gmail.com>
2014-12-04 08:29:41 -0500

    finished countdown to tuesday

---
+++ app/views/layouts/landing.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  {{ HTML::style('css/main.css') }}
+  {{ HTML::script('js/jquery-2.1.1.min.js') }}
+  {{ HTML::script('js/moment.min.js') }}
+</head>
+<body>
+  <div class="wrapper">
+    @yield('content')
+  </div>
+</body>
+</html>
