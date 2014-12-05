c43bb2a202cce4d44d031d77c68a6f49e641a966
NathanTcz <nathancazell@gmail.com>
2014-12-05 14:54:23 -0500

    base message feed

--- app/views/landing/index.blade.php
+++ app/views/landing/index.blade.php
@@ -5,6 +5,26 @@
   <div class="clock">
     <span id="time"></span>
   </div>
+  <div class="feed">
+  </div>
+
+  <form action="">
+    <input id="m" autocomplete="off" /><button>Send</button>
+  </form>
 
   {{ HTML::script('js/countdown.js') }}
+  {{ HTML::script('https://cdn.socket.io/socket.io-1.2.0.js') }}
+  <script>
+  var socket = io('http://localhost:3000');
+
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    $('.feed').append($('<li>').text(msg));
+  });
+  </script>
 @stop
--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,12 +1,12 @@
 function recalc() {
   var Now = moment();
-  var next_tue = moment().day(2).startOf('day').add('days',7)
+  var next_tue = moment().day(2).startOf('day').add(7, 'days');
 
-  diff = Now.diff(next_tue, 'seconds'),
+  diff = Now.diff(next_tue, 'seconds');
   diff = Math.abs(diff);
-  hours = Math.floor(diff / 3600),
-  diff = diff - hours * 3600,
-  minutes = Math.floor(diff / 60),
+  hours = Math.floor(diff / 3600);
+  diff = diff - hours * 3600;
+  minutes = Math.floor(diff / 60);
   seconds = diff - minutes * 60;
 
   (hours < 10) ? hours = '0' + hours : hours;
