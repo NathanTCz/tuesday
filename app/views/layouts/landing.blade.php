2b0ec3f56331b7a64a593aa24e828940b9f4d251
NathanTcz <nathancazell@gmail.com>
2014-12-05 10:37:11 -0500

    changed ga.js include

--- app/views/layouts/landing.blade.php
+++ app/views/layouts/landing.blade.php
@@ -4,16 +4,7 @@
   {{ HTML::style('css/main.css') }}
   {{ HTML::script('js/jquery-2.1.1.min.js') }}
   {{ HTML::script('js/moment.min.js') }}
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-57379303-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+  {{ HTML::script('js/ga.js') }}
 </head>
 <body>
   <div class="wrapper">
