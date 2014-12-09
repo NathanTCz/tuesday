e02459daddb3b1f5af53a1204c3a6536fe37d41d
NathanTcz <nathancazell@gmail.com>
2014-12-09 18:25:51 -0500

    add files

---
+++ app/views/layouts/mobile.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+  {{ HTML::style('css/mobile.css') }}
+  {{ HTML::script('js/jquery-2.1.1.min.js') }}
+  {{ HTML::script('js/moment.min.js') }}
+  {{ HTML::script('js/ga.js') }}
+</head>
+<body>
+  <div class="wrapper">
+    @yield('content')
+  </div>
+  {{ HTML::script('js/skrollr.min.js') }}
+  <script>
+    var s = skrollr.init({forceHeight:false});
+  </script>
+</body>
+</html>
